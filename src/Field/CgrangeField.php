803cfcd073eb796be4e80ca7d30c5bcafc6b9e55
Pascal Leconte <pascal.leconte@conseilgouz.com>
2024-04-10 11:27:20 +0200

    4.3.5
    CG Range : missing function collectLayoutData

--- src/Field/CgrangeField.php
+++ src/Field/CgrangeField.php
@@ -1,7 +1,7 @@
 <?php
 /**
 * CG Scroll - Joomla Module
-* Version			: 4.3.4
+* Version			: 4.3.5
 * Package			: Joomla 3.10.x - 4.x - 5.x
 * copyright 		: Copyright (C) 2024 ConseilGouz. All rights reserved.
 * license    		: https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL
@@ -12,7 +12,6 @@ namespace ConseilGouz\Module\CGScroll\Site\Field;
 defined('JPATH_PLATFORM') or die;
 use Joomla\CMS\Form\Field\RangeField;
 use Joomla\CMS\Factory;
-use Joomla\Component\Modules\Administrator\Model\ModuleModel;
 
 class CgrangeField extends RangeField
 {
@@ -41,5 +40,23 @@ class CgrangeField extends RangeField
         $wa->registerAndUseScript('cgrange', 'media/mod_cg_scroll/js/cgrange.js');
 
         return $this->getRenderer($this->layout)->render($this->collectLayoutData());
+    }
+    /**
+     * Method to get the data to be passed to the layout for rendering.
+     * The data is cached in memory.
+     *
+     * @return  array
+     *
+     * @since 5.1.0
+     */
+    protected function collectLayoutData(): array
+    {
+        if ($this->layoutData) {
+            return $this->layoutData;
+        }
+
+        $this->layoutData = $this->getLayoutData();
+
+        return $this->layoutData;
     }
 }
