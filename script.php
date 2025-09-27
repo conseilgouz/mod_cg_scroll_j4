8949fd1e4c45ce12bebafeba0edf3119fe81ffe5
Pascal Leconte <pascal.leconte@conseilgouz.com>
2025-09-27 15:41:00 +0200

    3.6.1
    Use ConseilGouz Library
    Joomla 6 compatibility

--- script.php
+++ script.php
@@ -1,6 +1,6 @@
 <?php
 /**
-* CG Scroll Module  - Joomla 4.x/5.x/6.x Module 
+* CG Scroll Module  - Joomla 4.x/5.x/6.x Module
 * Package			: CG Scroll
 * copyright 		: Copyright (C) 2025 ConseilGouz. All rights reserved.
 * license    		: http://www.gnu.org/licenses/gpl-2.0.html GNU/GPL
@@ -16,50 +16,47 @@ use Joomla\Database\DatabaseInterface;
 
 class mod_cg_scrollInstallerScript
 {
-	private $min_joomla_version      = '4.0.0';
-	private $min_php_version         = '7.4';
-	private $name                    = 'CG Scroll';
-	private $exttype                 = 'module';
-	private $extname                 = 'cg_scroll';
-	private $previous_version        = '';
+    private $min_joomla_version      = '4.0.0';
+    private $min_php_version         = '7.4';
+    private $name                    = 'CG Scroll';
+    private $exttype                 = 'module';
+    private $extname                 = 'cg_scroll';
+    private $previous_version        = '';
     private $newlib_version	         = '';
-	private $dir           = null;
-	private $lang;
-	private $installerName = 'cg_flipinstaller';
-	public function __construct()
-	{
-		$this->dir = __DIR__;
-		$this->lang = Factory::getApplication()->getLanguage();
-		$this->lang->load($this->extname);
-	}
-
-    function preflight($type, $parent)
+    private $dir           = null;
+    private $lang;
+    private $installerName = 'cg_flipinstaller';
+    public function __construct()
     {
-		if ( ! $this->passMinimumJoomlaVersion())
-		{
-			$this->uninstallInstaller();
-			return false;
-		}
-
-		if ( ! $this->passMinimumPHPVersion())
-		{
-			$this->uninstallInstaller();
-			return false;
-		}
-		// To prevent installer from running twice if installing multiple extensions
-		if ( ! file_exists($this->dir . '/' . $this->installerName . '.xml'))
-		{
-			return true;
-		}
-		$xml = simplexml_load_file(JPATH_BASE . '/modules/mod_'.$this->extname.'/mod_'.$this->extname.'.xml');
-		$this->previous_version = $xml->version;
+        $this->dir = __DIR__;
+        $this->lang = Factory::getApplication()->getLanguage();
+        $this->lang->load($this->extname);
     }
-    
-    function postflight($type, $parent)
+
+    public function preflight($type, $parent)
     {
-		if (($type=='install') || ($type == 'update')) { // remove obsolete dir/files
-			$this->postinstall_cleanup();
-		}
+        if (! $this->passMinimumJoomlaVersion()) {
+            $this->uninstallInstaller();
+            return false;
+        }
+
+        if (! $this->passMinimumPHPVersion()) {
+            $this->uninstallInstaller();
+            return false;
+        }
+        // To prevent installer from running twice if installing multiple extensions
+        if (! file_exists($this->dir . '/' . $this->installerName . '.xml')) {
+            return true;
+        }
+        $xml = simplexml_load_file(JPATH_BASE . '/modules/mod_'.$this->extname.'/mod_'.$this->extname.'.xml');
+        $this->previous_version = $xml->version;
+    }
+
+    public function postflight($type, $parent)
+    {
+        if (($type == 'install') || ($type == 'update')) { // remove obsolete dir/files
+            $this->postinstall_cleanup();
+        }
         if (!$this->checkLibrary('conseilgouz')) { // need library installation
             $ret = $this->installPackage('lib_conseilgouz');
             if ($ret) {
@@ -74,110 +71,109 @@ class mod_cg_scrollInstallerScript
             JPATH_SITE . '/media/mod_'.$this->extname.'/js/cgrange.js',
         ]);
 
-		switch ($type) {
-            case 'install': $message = Text::_('ISO_POSTFLIGHT_INSTALLED'); break;
-            case 'uninstall': $message = Text::_('ISO_POSTFLIGHT_UNINSTALLED'); break;
-            case 'update': $message = Text::_('ISO_POSTFLIGHT_UPDATED'); break;
-            case 'discover_install': $message = Text::_('ISO_POSTFLIGHT_DISC_INSTALLED'); break;
+        switch ($type) {
+            case 'install': $message = Text::_('ISO_POSTFLIGHT_INSTALLED');
+                break;
+            case 'uninstall': $message = Text::_('ISO_POSTFLIGHT_UNINSTALLED');
+                break;
+            case 'update': $message = Text::_('ISO_POSTFLIGHT_UPDATED');
+                break;
+            case 'discover_install': $message = Text::_('ISO_POSTFLIGHT_DISC_INSTALLED');
+                break;
         }
-		return true;
+        return true;
     }
-	private function postinstall_cleanup() {
+    private function postinstall_cleanup()
+    {
         // move layout to its correct directory
         $f = JPATH_SITE . "/layouts/conseilgouz";
         if (!is_dir($f)) {
             mkdir($f);
         }
-        copy(JPATH_SITE."/modules/mod_cg_scroll/layouts/cgrange.php",$f.'/'."cgrange.php");
-        
-		$obsloteFolders = ['css', 'js','language','layouts'];
-		// Remove plugins' files which load outside of the component. If any is not fully updated your site won't crash.
-		foreach ($obsloteFolders as $folder)
-		{
-			$f = JPATH_SITE . '/modules/mod_'.$this->extname.'/' . $folder;
+        copy(JPATH_SITE."/modules/mod_cg_scroll/layouts/cgrange.php", $f.'/'."cgrange.php");
 
-			if (!@file_exists($f) || !is_dir($f) || is_link($f))
-			{
-				continue;
-			}
+        $obsloteFolders = ['css', 'js','language','layouts'];
+        // Remove plugins' files which load outside of the component. If any is not fully updated your site won't crash.
+        foreach ($obsloteFolders as $folder) {
+            $f = JPATH_SITE . '/modules/mod_'.$this->extname.'/' . $folder;
 
-			Folder::delete($f);
-		}
-		$obsloteFiles = [sprintf("%s/modules/mod_%s/helper.php", JPATH_SITE, $this->extname),
-                         sprintf("%s/modules/mod_%s/mod_%s.php", JPATH_SITE, $this->extname,$this->extname)];
-		foreach ($obsloteFiles as $file)
-		{
-			if (@is_file($file))
-			{
-				File::delete($file);
-			}
-		}
-		$j = new Version();
-		$version=$j->getShortVersion(); 
-		$version_arr = explode('.',$version);
-		if (($version_arr[0] >= "4") || (($version_arr[0] == "3") && ($version_arr[1] == "10"))) {
-			// Delete 3.9 and older language files
-			$langFiles = [
-				sprintf("%s/language/en-GB/en-GB.mod_%s.ini", JPATH_SITE, $this->extname),
-				sprintf("%s/language/en-GB/en-GB.mod_%s.sys.ini", JPATH_SITE, $this->extname),
-				sprintf("%s/language/fr-FR/fr-FR.mod_%s.ini", JPATH_SITE, $this->extname),
-				sprintf("%s/language/fr-FR/fr-FR.mod_%s.sys.ini", JPATH_SITE, $this->extname),
-			];
-			foreach ($langFiles as $file) {
-				if (@is_file($file)) {
-					File::delete($file);
-				}
-			}
-		}
-		// remove obsolete update sites
-		$db = Factory::getContainer()->get(DatabaseInterface::class);
-		$query = $db->getQuery(true)
-			->delete('#__update_sites')
-			->where($db->quoteName('location') . ' like "%432473037d.url-de-test.ws/%"');
-		$db->setQuery($query);
-		$db->execute();
-		// Simple Isotope is now on Github
-		$query = $db->getQuery(true)
-			->delete('#__update_sites')
-			->where($db->quoteName('location') . ' like "%conseilgouz.com/updates/cg_scroll%"');
-		$db->setQuery($query);
-		$db->execute();
-		
-	}
+            if (!@file_exists($f) || !is_dir($f) || is_link($f)) {
+                continue;
+            }
 
-	// Check if Joomla version passes minimum requirement
-	private function passMinimumJoomlaVersion()
-	{
-		$j = new Version();
-		$version=$j->getShortVersion(); 
-		if (version_compare($version, $this->min_joomla_version, '<'))
-		{
-			Factory::getApplication()->enqueueMessage(
-				'Incompatible Joomla version : found <strong>' . $version . '</strong>, Minimum : <strong>' . $this->min_joomla_version . '</strong>',
-				'error'
-			);
+            Folder::delete($f);
+        }
+        $obsloteFiles = [sprintf("%s/modules/mod_%s/helper.php", JPATH_SITE, $this->extname),
+                         sprintf("%s/modules/mod_%s/mod_%s.php", JPATH_SITE, $this->extname, $this->extname)];
+        foreach ($obsloteFiles as $file) {
+            if (@is_file($file)) {
+                File::delete($file);
+            }
+        }
+        $j = new Version();
+        $version = $j->getShortVersion();
+        $version_arr = explode('.', $version);
+        if (($version_arr[0] >= "4") || (($version_arr[0] == "3") && ($version_arr[1] == "10"))) {
+            // Delete 3.9 and older language files
+            $langFiles = [
+                sprintf("%s/language/en-GB/en-GB.mod_%s.ini", JPATH_SITE, $this->extname),
+                sprintf("%s/language/en-GB/en-GB.mod_%s.sys.ini", JPATH_SITE, $this->extname),
+                sprintf("%s/language/fr-FR/fr-FR.mod_%s.ini", JPATH_SITE, $this->extname),
+                sprintf("%s/language/fr-FR/fr-FR.mod_%s.sys.ini", JPATH_SITE, $this->extname),
+            ];
+            foreach ($langFiles as $file) {
+                if (@is_file($file)) {
+                    File::delete($file);
+                }
+            }
+        }
+        // remove obsolete update sites
+        $db = Factory::getContainer()->get(DatabaseInterface::class);
+        $query = $db->getQuery(true)
+            ->delete('#__update_sites')
+            ->where($db->quoteName('location') . ' like "%432473037d.url-de-test.ws/%"');
+        $db->setQuery($query);
+        $db->execute();
+        // Simple Isotope is now on Github
+        $query = $db->getQuery(true)
+            ->delete('#__update_sites')
+            ->where($db->quoteName('location') . ' like "%conseilgouz.com/updates/cg_scroll%"');
+        $db->setQuery($query);
+        $db->execute();
 
-			return false;
-		}
+    }
 
-		return true;
-	}
+    // Check if Joomla version passes minimum requirement
+    private function passMinimumJoomlaVersion()
+    {
+        $j = new Version();
+        $version = $j->getShortVersion();
+        if (version_compare($version, $this->min_joomla_version, '<')) {
+            Factory::getApplication()->enqueueMessage(
+                'Incompatible Joomla version : found <strong>' . $version . '</strong>, Minimum : <strong>' . $this->min_joomla_version . '</strong>',
+                'error'
+            );
 
-	// Check if PHP version passes minimum requirement
-	private function passMinimumPHPVersion()
-	{
+            return false;
+        }
 
-		if (version_compare(PHP_VERSION, $this->min_php_version, '<'))
-		{
-			Factory::getApplication()->enqueueMessage(
-					'Incompatible PHP version : found  <strong>' . PHP_VERSION . '</strong>, Minimum <strong>' . $this->min_php_version . '</strong>',
-				'error'
-			);
-			return false;
-		}
+        return true;
+    }
 
-		return true;
-	}
+    // Check if PHP version passes minimum requirement
+    private function passMinimumPHPVersion()
+    {
+
+        if (version_compare(PHP_VERSION, $this->min_php_version, '<')) {
+            Factory::getApplication()->enqueueMessage(
+                'Incompatible PHP version : found  <strong>' . PHP_VERSION . '</strong>, Minimum <strong>' . $this->min_php_version . '</strong>',
+                'error'
+            );
+            return false;
+        }
+
+        return true;
+    }
     private function checkLibrary($library)
     {
         $file = $this->dir.'/lib_conseilgouz/conseilgouz.xml';
@@ -213,26 +209,28 @@ class mod_cg_scrollInstallerScript
         $installed = $tmpInstaller->install($this->dir . '/' . $package);
         return $installed;
     }
-    
-	private function uninstallInstaller()
-	{
-		if ( ! is_dir(JPATH_PLUGINS . '/system/' . $this->installerName)) {
-			return;
-		}
-		$this->delete([
-			JPATH_PLUGINS . '/system/' . $this->installerName . '/language',
-			JPATH_PLUGINS . '/system/' . $this->installerName,
-		]);
-		$db = Factory::getContainer()->get(DatabaseInterface::class);
-		$query = $db->getQuery(true)
-			->delete('#__extensions')
-			->where($db->quoteName('element') . ' = ' . $db->quote($this->installerName))
-			->where($db->quoteName('folder') . ' = ' . $db->quote('system'))
-			->where($db->quoteName('type') . ' = ' . $db->quote('plugin'));
-		$db->setQuery($query);
-		$db->execute();
-		Factory::getCache()->clean('_system');
-	}
+
+    private function uninstallInstaller()
+    {
+        if (! is_dir(JPATH_PLUGINS . '/system/' . $this->installerName)) {
+            return;
+        }
+        $this->delete([
+            JPATH_PLUGINS . '/system/' . $this->installerName . '/language',
+            JPATH_PLUGINS . '/system/' . $this->installerName,
+        ]);
+        $db = Factory::getContainer()->get(DatabaseInterface::class);
+        $query = $db->getQuery(true)
+            ->delete('#__extensions')
+            ->where($db->quoteName('element') . ' = ' . $db->quote($this->installerName))
+            ->where($db->quoteName('folder') . ' = ' . $db->quote('system'))
+            ->where($db->quoteName('type') . ' = ' . $db->quote('plugin'));
+        $db->setQuery($query);
+        $db->execute();
+        $cache = Factory::getContainer()->get(Joomla\CMS\Cache\CacheControllerFactoryInterface::class)->createCacheController();
+        $cache->clean('_system');
+
+    }
     public function delete($files = [])
     {
         foreach ($files as $file) {
--- tmpl/default.php
+++ tmpl/default.php
@@ -1,6 +1,6 @@
 <?php
 /**
-* CG Scroll - Joomla Module 
+* CG Scroll - Joomla Module
 * Package			: 4.x - 5.x
 * copyright 		: Copyright (C) 2025 ConseilGouz. All rights reserved.
 * license    		: https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL
@@ -26,60 +26,69 @@ $sf_type = $params->get('sf_type', 'FEED');
 $sf_height	= $params->get('sf_height', 200);
 $sf_width   = $params->get('sf_width', 200);
 if ($sf_type == 'FEED') {
-	$rssitems   = $params->get('rssitems',3);
+    $rssitems   = $params->get('rssitems', 3);
 } else {
-	$rssitems   = $params->get('catitems',3);
+    $rssitems   = $params->get('catitems', 3);
 }
 $sf_delay	= $params->get('sf_delay', 1);
 $sf_speed	= $params->get('sf_speed', 2);
 $sf_pause	= $params->get('sf_pause', 1);
-$sf_w_img	= $params->get('sf_w_img', '100'); 
+$sf_w_img	= $params->get('sf_w_img', '100');
 $rssurl		= $params->get('rssurl', '');
 $rssrtl		= $params->get('rssrtl', 0);
 $sf_direction = $params->get('direction', 0);
 $sf_delay	= $sf_delay * 1000;
-$sf_w_img   = str_replace('%','',$sf_w_img);   
-$sf_wimg_responsive=(100 - $sf_w_img)/2;
+$sf_w_img   = str_replace('%', '', $sf_w_img);
+$sf_wimg_responsive = (100 - $sf_w_img) / 2;
 $sf_slowdown = $params->get('sf_extraslow', 0);
 
-if ($sf_w_img>='55'): $margin_item_image='padding: 0 '.$sf_wimg_responsive.'% ;'; endif; 
-if ($sf_w_img<='54'): $margin_item_image="margin-right:5px; float:left;"; endif;         
+if ($sf_w_img >= '55'): $margin_item_image = 'padding: 0 '.$sf_wimg_responsive.'% ;'; endif;
+if ($sf_w_img <= '54'): $margin_item_image = "margin-right:5px; float:left;"; endif;
 
-$wa = Factory::getDocument()->getWebAssetManager();
-$wa->registerAndUseStyle('scroll','media/'.$module->module.'/css/scroll.css');
-if ((bool)Factory::getConfig()->get('debug')) { // Mode debug
-    $document->addScript($modulefield.'js/scroll.js'); 
-} else {
-    $wa->registerAndUseScript('scroll','media/'.$module->module.'/js/scroll.js');
-}
+$wa = $document->getWebAssetManager();
+$wa->registerAndUseStyle('scroll', 'media/'.$module->module.'/css/scroll.css');
+$wa->registerAndUseScript('scroll', 'media/'.$module->module.'/js/scroll.js');
 if ($sf_type == 'FEED') {
-	$feed = CGScrollHelper::getFeed($params);
-	$count = $params->get('rssitems', 5);
-	if (is_array($feed) && ($count > count($feed)))  { $count = count($feed);}
-	$document->addScriptOptions('mod_cg_scroll_'.$module->id, 
-                array('id' => $module->id,'speed' => $sf_speed, 'pause' => $sf_pause, 'height' => $sf_height, 'width' => $sf_width, 'direction' => $sf_direction,'count'=> $count,'delay'=>$sf_delay,'slowdown'=>$sf_slowdown));
-	require ModuleHelper::getLayoutPath('mod_cg_scroll', 'defaultFeed');
+    $feed = CGScrollHelper::getFeed($params);
+    $count = $params->get('rssitems', 5);
+    if (is_array($feed) && ($count > count($feed))) {
+        $count = count($feed);
+    }
+    $document->addScriptOptions(
+        'mod_cg_scroll_'.$module->id,
+        array('id' => $module->id,'speed' => $sf_speed, 'pause' => $sf_pause, 'height' => $sf_height, 'width' => $sf_width, 'direction' => $sf_direction,'count' => $count,'delay' => $sf_delay,'slowdown' => $sf_slowdown)
+    );
+    require ModuleHelper::getLayoutPath('mod_cg_scroll', 'defaultFeed');
 } elseif ($sf_type == 'CATEGORY') {
-	$category = $params->get('category_id',1);
-	$article = CGScrollHelper::getCategory($category,$params);
-	$count = $params->get('catitems', 5);
-	if ($count > count($article))  { $count = count($article);}
-	$document->addScriptOptions($module->module.'_'.$module->id, 
-                array('id' => $module->id,'speed' => $sf_speed, 'pause' => $sf_pause, 'height' => $sf_height, 'width' => $sf_width, 'direction' => $sf_direction,'count'=> $count,'delay'=>$sf_delay,'slowdown'=>$sf_slowdown));
-	require ModuleHelper::getLayoutPath($module->module, 'defaultArticle');
+    $category = $params->get('category_id', 1);
+    $article = CGScrollHelper::getCategory($category, $params);
+    $count = $params->get('catitems', 5);
+    if ($count > count($article)) {
+        $count = count($article);
+    }
+    $document->addScriptOptions(
+        $module->module.'_'.$module->id,
+        array('id' => $module->id,'speed' => $sf_speed, 'pause' => $sf_pause, 'height' => $sf_height, 'width' => $sf_width, 'direction' => $sf_direction,'count' => $count,'delay' => $sf_delay,'slowdown' => $sf_slowdown)
+    );
+    require ModuleHelper::getLayoutPath($module->module, 'defaultArticle');
 } elseif ($sf_type == 'LATEST') {
-	$categories = $params->get('categories_id');
-	$article = CGScrollHelper::getLatest($categories,$params);
-	$count = $params->get('catitems', 5);
-	if ($count > count($article))  { $count = count($article);}
-	$document->addScriptOptions($module->module.'_'.$module->id, 
-               array('id' => $module->id,'speed' => $sf_speed, 'pause' => $sf_pause, 'height' => $sf_height, 'width' => $sf_width, 'direction' => $sf_direction,'count'=> $count,'delay'=>$sf_delay,'slowdown'=>$sf_slowdown));
-	require ModuleHelper::getLayoutPath($module->module, 'defaultArticle');
+    $categories = $params->get('categories_id');
+    $article = CGScrollHelper::getLatest($categories, $params);
+    $count = $params->get('catitems', 5);
+    if ($count > count($article)) {
+        $count = count($article);
+    }
+    $document->addScriptOptions(
+        $module->module.'_'.$module->id,
+        array('id' => $module->id,'speed' => $sf_speed, 'pause' => $sf_pause, 'height' => $sf_height, 'width' => $sf_width, 'direction' => $sf_direction,'count' => $count,'delay' => $sf_delay,'slowdown' => $sf_slowdown)
+    );
+    require ModuleHelper::getLayoutPath($module->module, 'defaultArticle');
 } else {
-	$articleId = $params->get('article_id',1);
-	$article = CGScrollHelper::getArticle($articleId,$params);
-	$document->addScriptOptions($module->module.'_'.$module->id, 
-                array('id' => $module->id,'speed' => $sf_speed, 'pause' => $sf_pause, 'height' => $sf_height, 'width' => $sf_width, 'direction' => $sf_direction,'count'=> 1,'delay'=>$sf_delay,'slowdown'=>$sf_slowdown));
-	require ModuleHelper::getLayoutPath($module->module, 'defaultArticle');
+    $articleId = $params->get('article_id', 1);
+    $article = CGScrollHelper::getArticle($articleId, $params);
+    $document->addScriptOptions(
+        $module->module.'_'.$module->id,
+        array('id' => $module->id,'speed' => $sf_speed, 'pause' => $sf_pause, 'height' => $sf_height, 'width' => $sf_width, 'direction' => $sf_direction,'count' => 1,'delay' => $sf_delay,'slowdown' => $sf_slowdown)
+    );
+    require ModuleHelper::getLayoutPath($module->module, 'defaultArticle');
 }
-?>
